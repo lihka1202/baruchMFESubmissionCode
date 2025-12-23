b1f0146051d2cf106d92616fad4b7acac6dc988b
lihka1202 <mnakhilwork@gmail.com>
2025-12-23 14:48:27 +0800

    chore: update template code

---
+++ Level 4/2.4/Exercise 1/Exercise 1/Point.hpp
@@ -0,0 +1,17 @@
+//
+//  Point.hpp
+//  Exercise 1
+//
+//  Created by Akhil Muthya Narayanachary on 23/12/25.
+//
+
+class Point {
+    double X;
+    double Y;
+    
+public:
+    double X();
+    double Y();
+    void X(double x);
+    void Y(double y);
+};
---
+++ Level 4/2.4/Exercise 1/Exercise 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Exercise 1
+//
+//  Created by Akhil Muthya Narayanachary on 23/12/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
